7e7afd52265b0162e42c40e75adcc1656be26fc4
joaop <joaopedrosao@gmail.com>
2023-08-10 08:59:34 -0300

    request creator button error solved.

---
+++ src/main/java/autototem/SystemTest.java
@@ -0,0 +1,13 @@
+package autototem;
+
+import java.io.File;
+import java.sql.SQLOutput;
+
+public class SystemTest {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+
+
+    }
+}
+
